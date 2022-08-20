c602c835cdf43b45788175bf038509a99a9b3aa3
nicolas ciuffi <nicolasciuffi1@gmail.com>
2022-08-20 10:53:43 -0300

    config changes

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,4 +1,5 @@
 export const API_URL = 'https://blog-strapi-latest.herokuapp.com';
 export const POSTS_URL = `${API_URL}/api/posts?populate=*`;
 export const CATEGORIES_URL = `${API_URL}/api/categories?populate=*`;
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL =
+  process.env.SITE_URL || 'https://nickciuffi-nextjs-blog.netlify.app';
