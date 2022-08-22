b3a11610303550d2c70b9871ab4c3b1f99fbd757
nicolas ciuffi <nicolasciuffi1@gmail.com>
2022-08-22 09:04:26 -0300

    agr vai meno

--- src/Comments/index.tsx
+++ src/Comments/index.tsx
@@ -1,4 +1,5 @@
-import Disqus from 'disqus-react';
+import { DiscussionEmbed } from 'disqus-react';
+import { useEffect, useState } from 'react';
 import { SITE_URL } from '../config/app-config';
 import { Container } from './style';
 
@@ -8,17 +9,16 @@ type CommentsProps = {
 };
 
 export function Comments({ slug, title }: CommentsProps) {
+  const disqusShortname = 'blog-next-4';
+  const disqusConfig = {
+    url: `${SITE_URL}/post/${slug}`,
+    identifier: slug, // Single post id
+    title: title, // Single post title
+  };
+
   return (
     <Container>
-      <Disqus.DiscussionEmbed
-        shortname="blog-next-4"
-        config={{
-          url: `${SITE_URL}/post/${slug}`,
-          identifier: slug,
-          title: title,
-          language: 'pt_BR',
-        }}
-      />
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
     </Container>
   );
 }
